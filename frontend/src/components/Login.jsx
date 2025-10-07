60a05aec57deedffe7bd81094d41112e6177c4fe
Rametyaverma <vermarametya@gmail.com>
2025-10-07 11:16:24 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = ({ onLogin }) => {
     e.preventDefault();
 
     try {
-      const res = await fetch(`${https://userauthchat-6.onrender.com}/api/auth/login`, {
+      const res = await fetch(`https://userauthchat-6.onrender.com/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
